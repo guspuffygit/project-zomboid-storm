882d577a4a2b69eb941fd0ced28c0eef70b15e49
guspuffygit <guspuffy@gmail.com>
2026-03-20 15:00:42 -0400

    Use getters and optional

--- src/main/java/io/pzstorm/storm/event/lua/OnClientCommandEvent.java
+++ src/main/java/io/pzstorm/storm/event/lua/OnClientCommandEvent.java
@@ -1,6 +1,10 @@
 package io.pzstorm.storm.event.lua;
 
+import java.util.Optional;
+
 import io.pzstorm.storm.event.core.LuaEvent;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.Nullable;
 import se.krka.kahlua.vm.KahluaTable;
 import zombie.characters.IsoPlayer;
@@ -9,21 +13,21 @@ import zombie.characters.IsoPlayer;
  * Triggered on the server when it receives a command from a client via
  * {@code sendClientCommand(module, command, args)}.
  *
- * <p>{@code args} may be {@code null} when the client sends a command with no arguments.
- * Event handlers should null-check before accessing it.
+ * <p>{@code args} may be empty when the client sends a command with no arguments.
  */
+@RequiredArgsConstructor
 @SuppressWarnings({"WeakerAccess", "unused"})
 public class OnClientCommandEvent implements LuaEvent {
 
-    public final String module;
-    public final String command;
-    public final IsoPlayer player;
-    public final @Nullable KahluaTable args;
+    @Getter
+    private final String module;
+    @Getter
+    private final String command;
+    @Getter
+    private final IsoPlayer player;
+    private final @Nullable KahluaTable args;
 
-    public OnClientCommandEvent(String module, String command, IsoPlayer player, @Nullable KahluaTable args) {
-        this.module = module;
-        this.command = command;
-        this.player = player;
-        this.args = args;
+    public Optional<KahluaTable> getArgs() {
+        return Optional.ofNullable(args);
     }
 }
