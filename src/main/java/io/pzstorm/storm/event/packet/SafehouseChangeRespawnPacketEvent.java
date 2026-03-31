8dd2a9f837604fc402e95a3a9f050c353f58d4c9
guspuffygit <guspuffy@gmail.com>
2026-03-31 13:03:15 -0400

    Add info to change respawn and change title for pre state

--- src/main/java/io/pzstorm/storm/event/packet/SafehouseChangeRespawnPacketEvent.java
+++ src/main/java/io/pzstorm/storm/event/packet/SafehouseChangeRespawnPacketEvent.java
@@ -10,12 +10,35 @@ import zombie.network.packets.safehouse.SafehouseChangeRespawnPacket;
  */
 public class SafehouseChangeRespawnPacketEvent extends PacketEvent {
 
+    private boolean wasRespawning;
+
     public SafehouseChangeRespawnPacketEvent(Object packet, UdpConnection connection) {
         super(packet, connection);
     }
 
     public SafehouseChangeRespawnPacket getPacket() {
         return (SafehouseChangeRespawnPacket) getRawPacket();
+    }
+
+    @Override
+    public void capturePreState() {
+        SafeHouse safehouse = getSafehouse();
+        if (safehouse != null) {
+            wasRespawning = safehouse.isRespawnInSafehouse(getPlayer());
+        }
+    }
+
+    public boolean wasRespawning() {
+        return wasRespawning;
+    }
+
+    /**
+     * Returns whether this packet will add the player to the respawn list. Note: the underlying
+     * packet field {@code doRemove} is misnamed — {@code true} means the player is being added, not
+     * removed.
+     */
+    public boolean isAddingRespawn() {
+        return getPacket().doRemove;
     }
 
     @Override
--- src/main/java/io/pzstorm/storm/event/packet/SafehouseChangeTitlePacketEvent.java
+++ src/main/java/io/pzstorm/storm/event/packet/SafehouseChangeTitlePacketEvent.java
@@ -1,5 +1,6 @@
 package io.pzstorm.storm.event.packet;
 
+import lombok.Getter;
 import zombie.core.raknet.UdpConnection;
 import zombie.iso.areas.SafeHouse;
 import zombie.network.packets.safehouse.SafehouseChangeTitlePacket;
@@ -10,12 +11,22 @@ import zombie.network.packets.safehouse.SafehouseChangeTitlePacket;
  */
 public class SafehouseChangeTitlePacketEvent extends PacketEvent {
 
+    @Getter private String previousTitle;
+
     public SafehouseChangeTitlePacketEvent(Object packet, UdpConnection connection) {
         super(packet, connection);
     }
 
     public SafehouseChangeTitlePacket getPacket() {
         return (SafehouseChangeTitlePacket) getRawPacket();
+    }
+
+    @Override
+    public void capturePreState() {
+        SafeHouse safehouse = getSafehouse();
+        if (safehouse != null) {
+            previousTitle = safehouse.getTitle();
+        }
     }
 
     @Override
