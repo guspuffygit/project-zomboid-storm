c0b8ea9a3fdda637f46c7b35453ef970c5d5e656
guspuffygit <guspuffy@gmail.com>
2026-04-01 08:05:02 -0400

    Add player to packet

--- src/main/java/io/pzstorm/storm/event/packet/NetTimedActionPacketEvent.java
+++ src/main/java/io/pzstorm/storm/event/packet/NetTimedActionPacketEvent.java
@@ -2,8 +2,11 @@ package io.pzstorm.storm.event.packet;
 
 import io.pzstorm.storm.lua.StormKahluaTable;
 import javax.annotation.Nullable;
+
+import zombie.characters.IsoPlayer;
 import zombie.core.raknet.UdpConnection;
 import zombie.network.PZNetKahluaTableImpl;
+import zombie.network.fields.character.PlayerID;
 import zombie.network.packets.NetTimedActionPacket;
 
 /**
@@ -23,6 +26,10 @@ public class NetTimedActionPacketEvent extends PacketEvent {
     @Override
     public String getName() {
         return "NetTimedActionPacketEvent";
+    }
+
+    public PlayerID getPlayerId() {
+        return (PlayerID) getField("playerId");
     }
 
     public String getActionType() {
